9950d92ce11bc4f048eb0bf93e09b68f11627852
Glitch (ecr-exercise-tracker) <none>
2019-12-14 17:03:25 +0000

    🌺🚿 Checkpoint ./server.js:9298160/1585

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MLAB_URI, { useNewUrlParser: true });
+console.log("mongoose connection readyState", mongoose.connection.readyState);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,31 +16,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({ status: 404, message: "not found" });
-});
 
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage;
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400; // bad request
-    const keys = Object.keys(err.errors);
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message;
-  } else {
-    // generic or custom error
-    errCode = err.status || 500;
-    errMessage = err.message || "Internal Server Error";
-  }
-  res
-    .status(errCode)
-    .type("txt")
-    .send(errMessage);
-});
 
 const UserSchema = new mongoose.Schema({
   username: String
@@ -57,13 +34,38 @@ const ExerciseModel = mongoose.model("ExerciseModel", ExerciseSchema);
 app.post("/api/exercise/new-user", (req, res) => {
   UserModel.findOne({ username: req.body.username }, (err, data) => {
     if (err) console.log(err);
-    console.log(data);
     if (data) {
       res.status(400).send("username already taken");
     } else {
-      res.status(400).send("username already taken111");
+      UserModel.create({username});
     }
   });
+});
+
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({ status: 404, message: "not found" });
+});
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || "Internal Server Error";
+  }
+  res
+    .status(errCode)
+    .type("txt")
+    .send(errMessage);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
